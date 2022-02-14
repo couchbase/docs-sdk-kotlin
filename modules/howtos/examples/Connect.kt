74ca33eab740cf6bb58ee99409ae72158b593187
David Nault <david.nault@couchbase.com>
2022-02-13 19:04:28 -0800

    connection docs cleanup

--- modules/howtos/examples/Connect.kt
+++ modules/howtos/examples/Connect.kt
@@ -3,11 +3,11 @@ import kotlinx.coroutines.runBlocking
 import kotlin.time.Duration.Companion.seconds
 
 fun main() {
-    val username = "Administrator"
-    val password = "password"
-    val connectionString = "127.0.0.1"
-
-    val cluster = Cluster.connect(connectionString, username, password)
+    val cluster = Cluster.connect(
+        connectionString = "127.0.0.1",
+        username = "Administrator",
+        password = "password",
+    )
 
     runBlocking { // <1>
         try {
