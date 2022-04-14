3582531303b3a227f40a892e3b1cd1cd8421283e
David Nault <david.nault@couchbase.com>
2022-04-13 17:15:55 -0700

    Document expiry

--- modules/howtos/examples/KvBasic.kt
+++ modules/howtos/examples/KvBasic.kt
@@ -1,7 +1,10 @@
+import com.couchbase.client.core.error.CasMismatchException
 import com.couchbase.client.core.error.DocumentExistsException
 import com.couchbase.client.core.error.DocumentNotFoundException
 import com.couchbase.client.kotlin.Collection
+import com.couchbase.client.kotlin.kv.Expiry
 import com.couchbase.client.kotlin.kv.GetResult
+import kotlin.time.Duration.Companion.hours
 
 /*
  * Copyright 2022 Couchbase, Inc.
@@ -76,4 +79,118 @@ private suspend fun get(collection: Collection) {
         println("Get failed because the document does not exist.")
     }
 // end::get[]
+}
+
+private suspend fun upsertWithExpiry(collection: Collection) {
+// tag::upsertWithExpiry[]
+    collection.upsert(
+        id = "alice",
+        content = mapOf("favoriteColor" to "red"),
+        expiry = Expiry.of(3.hours), // <1>
+    )
+// end::upsertWithExpiry[]
+}
+
+
+private suspend fun touch(collection: Collection) {
+// tag::touch[]
+    try {
+        collection.touch(
+            id = "alice",
+            expiry = Expiry.of(3.hours),
+        )
+    } catch (t: DocumentNotFoundException) {
+        println("Touch failed because the document does not exist.")
+    }
+// end::touch[]
+}
+
+private suspend fun getAndTouch(collection: Collection) {
+// tag::getAndTouch[]
+    try {
+        val result: GetResult = collection.getAndTouch(
+            id = "alice",
+            expiry = Expiry.of(3.hours), // <1>
+        )
+        val content = result.contentAs<Map<String, Any?>>()
+        println("The user's favorite color is ${content["favoriteColor"]}")
+    } catch (t: DocumentNotFoundException) {
+        println("GetAndTouch failed because the document does not exist.")
+    }
+// end::getAndTouch[]
+}
+
+private suspend fun replacePreserveExpiry(collection: Collection) {
+// tag::replacePreserveExpiry[]
+    collection.replace(
+        id = "alice",
+        content = mapOf("favoriteColor" to "red"),
+        preserveExpiry = true,
+    )
+// end::replacePreserveExpiry[]
+}
+
+
+private suspend fun replacePreserveExpiryHardWayLocking(collection: Collection) {
+
+// tag::replacePreserveExpiryHardWayLocking[]
+    val documentId = "alice"
+
+    while (true) {
+        val old = collection.get(
+            id = documentId,
+            withExpiry = true, // <1>
+        )
+
+        val oldContent = old.contentAs<Map<String, Any?>>()
+        val newContent = oldContent +
+                ("favoriteFood" to "hamburger")
+
+        try {
+            collection.replace(
+                id = documentId,
+                content = newContent,
+                expiry = old.expiry,
+                cas = old.cas, // <2>
+            )
+            break
+
+        } catch (_: CasMismatchException) {
+            // Someone else changed the document,
+            // and the expiry might have changed too.
+            // Start again.
+        }
+    }
+// end::replacePreserveExpiryHardWayLocking[]
+}
+
+private suspend fun replacePreserveExpiryHardWay(collection: Collection) {
+// tag::replacePreserveExpiryHardWay[]
+    val documentId = "alice"
+    val old = collection.get(
+        id = documentId,
+        withExpiry = true, // <1>
+    )
+    collection.replace(
+        id = documentId,
+        content = mapOf("favoriteColor" to "red"),
+        expiry = old.expiry,
+    )
+    // end::replacePreserveExpiryHardWay[]
+}
+
+
+private suspend fun getWithExpiry(collection: Collection) {
+// tag::getWithExpiry[]
+    val result: GetResult = collection.get(
+        id = "alice",
+        withExpiry = true, // <1>
+    )
+
+    when (val expiry = result.expiry) {
+        is Expiry.None -> println("Document does not expire.")
+        is Expiry.Absolute -> println("Document expires at ${expiry.instant}.")
+        else -> println("Oops, forgot to pass `withExpiry = true`.")
+    }
+// end::getWithExpiry[]
 }
